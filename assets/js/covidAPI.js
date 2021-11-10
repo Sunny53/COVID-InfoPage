fd0c7555f8358594288caa23bd062d88fe78ffd8
Sunny Patel <sunnykp5397@gmail.com>
2021-11-10 16:00:13 -0500

    News Dropdown works properly
    Render fetch API onLoad. Clear container onChange and render new API endpoint.

--- assets/js/covidAPI.js
+++ assets/js/covidAPI.js
@@ -44,25 +44,22 @@ fetch(url, {
 
 //Getting Data from Gnews API
 
-//Html elements
-
-
 //APi parameters
-var q = 'coronavirus savannah';
-const url1 = `https://gnews.io/api/v4/search?q=${q}&max=5&country=us&lang=en&token=870061f7bf29680986c7953224cb9bd3`
-console.log(url);
-
 let newsContainer = document.getElementById("news__container");
+let newsDropdown = document.getElementById('news__dropdown');
 
 //API call
+
+function newsfetch(q){
+
+const url1 = `https://gnews.io/api/v4/search?q=${q}&max=5&country=us&lang=en&token=870061f7bf29680986c7953224cb9bd3`
+console.log(url1);
+
 fetch(url1)
   .then(response => response.json())
   .then(data => {
     data.articles.map(i => {
-
-      console.log(i.title);
-
-
+      
       let newsArticle = document.createElement("div");
       newsArticle.classList.add('news__article');
       
@@ -82,19 +79,17 @@ fetch(url1)
   .catch(error => console.log("Error:", error))
 
   
-
-//Dropdown onchange
-let newsDropdown = document.getElementById('news__dropdown');
-
-newsDropdown.addEventListener('change', (e)=>{
-console.log(newsDropdown.value);
-
-const queryObject = {
-  local: 'coronavirus local',
-  state: 'coronavirus georgia',
-  national: 'coronavirus us',
-  global: 'coronavirus'
 }
 
-return q = queryObject[newsDropdown.value]
-})
+//fetch onload
+window.onload(newsfetch('coronavirus savannah'))
+
+
+//Dropdown onchange
+newsDropdown.addEventListener('change', (e)=>{
+  
+  newsContainer.innerHTML = " ";
+  q = e.target.value;
+  newsfetch(q);
+  })
+  
